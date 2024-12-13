034e29fb817202856a4cc435010087b5d94001fa
Oliwia <grabiecoliwia531@gmail.com>
2024-12-13 18:14:26 +0100

    Changes

---
+++ src/views/main-page/main-page.js
@@ -0,0 +1,12 @@
+import logo from "/logo.svg";
+
+function Main() {
+  return (
+    <header className="header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>FINSAVE</p>
+    </header>
+  );
+}
+
+export default Main;
